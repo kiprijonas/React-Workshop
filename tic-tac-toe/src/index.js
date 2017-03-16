dbd6cd15521a945b3b5ac0d716a0ac28a7a781c9
Zilvinas Simaitis <zsimaiti@lt.ibm.com>
2017-03-16 16:53:21 +0200

    step 5

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    const squares = this.props.squares;
+    return <Square value={squares[i]} onClick={() => this.props.onClick(i)} />;
   }
   render() {
-    const status = 'Next player: X';
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -41,26 +40,121 @@ class Board extends React.Component {
   }
 }
 
+const PLAY_MOVE = 'PLAY_MOVE';
+const JUMP_TO_HISTORY = 'JUMP_TO_HISTORY';
+
+function playMove(square_id) {
+  return {
+    type: PLAY_MOVE,
+    square_id: square_id
+  }
+}
+
+function jumpToHistory(step){
+  return {
+    type: JUMP_TO_HISTORY,
+    step: step
+  }
+}
+
+const initialState = {  history: [{squares: Array(9).fill(null)}],
+                              stepNumber: 0,
+                              xIsNext: true,
+                    }
+
+const gameReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case PLAY_MOVE:
+        var history = state.history.slice(0, state.stepNumber + 1);
+        var current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[action.square_id]) {
+          return state;
+        }
+        squares[action.square_id] = state.xIsNext ? 'X' : 'O';
+        return {
+          history: history.concat([{
+            squares: squares
+          }]),
+          stepNumber: history.length,
+          xIsNext: !state.xIsNext,
+        }
+        case JUMP_TO_HISTORY:
+        return {
+          ...state,
+          stepNumber: action.step,
+          xIsNext: (action.step % 2) ? false : true,
+        }
+        default: return state
+  }
+}
+
+
+const mapStateToProps = (store) => {
+  return {
+    history: store.history,
+    stepNumber: store.stepNumber,
+    xIsNext: store.xIsNext
+  };
+}
+
+let gameStore = createStore(gameReducer);
+
 class Game extends React.Component {
+  handleClick(i) {
+    gameStore.dispatch(playMove(i))
+  }
+  jumpTo(step) {
+    gameStore.dispatch(jumpToHistory(step))
+  }
   render() {
+    const history = this.props.history;
+    const current = history[this.props.stepNumber];
+
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+    }
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
     return (
       <div className="game">
-        <div className="game-board">
-          <Board />
+        <div>
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
 
+Game = connect(mapStateToProps)(Game)
+
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Provider store={gameStore}>
+    <Game />
+  </Provider>,
   document.getElementById('container')
 );
 
@@ -83,4 +177,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-
